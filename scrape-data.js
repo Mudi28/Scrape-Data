3aea4d16302430e38faaceab01e3c154d0867593
mudi28 <mudi2000j@gmail.com>
2023-03-03 13:59:54 +0100

    Chapter 4

--- scrape-data.js
+++ scrape-data.js
@@ -8,16 +8,16 @@ console.log("ESLint and Prettier are working on save!");
 console.log("ESLint and Prettier are working on save!");
 
 
-
 async function Scrapedata() {
   try {
     const response = await axios.get("https://scrapeme.live/shop/page/2/");
     const htmlContent = response.data;  
     const $ = cheerio.load(htmlContent);
-
-    // Seleziona gli elementi utilizzando i selettori CSS definiti
     const products = [];
-    $(".product").each((index, element) => {
+    const productElements = $(".product");
+
+    for (let i = 0; i < productElements.length; i++) {
+      const element = productElements[i];
       const product = {
         name: $(element).find(".woocommerce-loop-product__title").text(),
         image: $(element).find("img").attr("src"),
@@ -27,16 +27,18 @@ async function Scrapedata() {
         price: $(element).find("span.price").text(),
       };
       products.push(product);
-    });
+    }
 
-    // Itera sugli elementi dell'array `products` e stampa i dati di ciascun prodotto
-    products.forEach((product, index) => {
-      console.log(`Product ${index + 1}:`);
+
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      console.log(`Product ${i + 1}:`);
       console.log(`Name: ${product.name}`);
       console.log(`Image URL: ${product.image}`);
       console.log(`Page URL: ${product.url}`);
       console.log(`Price: ${product.price}`);
-    });
+    }
+
     console.log(products);
   } catch (error) {
     console.error(error);

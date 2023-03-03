1567cf7051f485a6c2fbb4637762e31ee26e46b5
mudi28 <mudi2000j@gmail.com>
2023-03-03 18:33:07 +0100

    new changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    extends: ['prettier'],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': 'error',
-    },
-};
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { semi: false }],
+  },
+}
--- scrape-data.js
+++ scrape-data.js
@@ -1,49 +1,49 @@
-import axios from 'axios';
-import { load } from 'cheerio';
+import axios from 'axios'
+import { load } from 'cheerio'
 
-console.log('Hello, World!');
+console.log('Hello, World!')
 
-console.log('ESLint and Prettier are working on save!');
+console.log('ESLint and Prettier are working on save!')
 
-console.log('ESLint and Prettier are working on save!');
+console.log('ESLint and Prettier are working on save!')
 // This function scrapes data using the axios and cheerio libraries.
 async function scrapeData() {
   try {
     // I make an HTTP GET request to the specified URL expecting a response.
-    const response = await axios.get('https://scrapeme.live/shop/page/2/');
+    const response = await axios.get('https://scrapeme.live/shop/page/2/')
     // I extract the HTML content from the request response.
-    const htmlContent = response.data;
+    const htmlContent = response.data
     // Loading HTML content in the cheerio document.
-    const $ = load(htmlContent);
+    const $ = load(htmlContent)
     // I create an empty array for the extracted products.
-    const products = [];
+    const products = []
     // Selects all DOM elements with the class 'product'..
-    const productElements = $('.product');
+    const productElements = $('.product')
     // I iterate through each product element.
     for (let i = 0; i < productElements.length; i++) {
       // I extract product name,image,url,price from the element.
-      const element = productElements[i];
-      const name = $(element).find('.woocommerce-loop-product__title').text();
-      const image = $(element).find('img').attr('src');
+      const element = productElements[i]
+      const name = $(element).find('.woocommerce-loop-product__title').text()
+      const image = $(element).find('img').attr('src')
       const url = $(element)
         .find('a.woocommerce-LoopProduct-link.woocommerce-loop-product__link')
-        .attr('href');
-      const price = $(element).find('span.price').text();
+        .attr('href')
+      const price = $(element).find('span.price').text()
       // I create a product object containing the extracted information.
       const product = {
         name: name,
         image: image,
         url: url,
         price: price,
-      };
+      }
       //I add the product object to the product array.
-      products.push(product);
+      products.push(product)
     }
 
-    console.log(products);
+    console.log(products)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-scrapeData();
+scrapeData()

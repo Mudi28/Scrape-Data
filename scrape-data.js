3ff632df6c0fc1a213a3b07d0236a3af1515b620
mudi28 <mudi2000j@gmail.com>
2023-03-07 00:28:17 +0100

    fix some problems

--- scrape-data.js
+++ scrape-data.js
@@ -1,15 +1,13 @@
 import axios from 'axios'
 import { load } from 'cheerio'
-import fs from 'fs'
 import minimist from 'minimist'
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
 
-// Parse command line arguments using minimist
-const args = minimist(process.argv.slice(2), {
-  string: ['export'], // 'export' as a string argument
-  alias: { e: 'export' }, // '-e' as an alias for 'export'
-})
-
-// This function scrapes data using the axios and cheerio libraries
+// create an empty array for the extracted products
+const products = []
+// this function scrapes data using the axios and cheerio libraries
 async function scrapeData() {
   try {
     // make an HTTP GET request to the specified URL expecting a response
@@ -18,8 +16,6 @@ async function scrapeData() {
     const htmlContent = response.data
     // Loading HTML content in the cheerio document
     const $ = load(htmlContent)
-    // create an empty array for the extracted products
-    const products = []
     // Selects all DOM elements with the class 'product'
     const productElements = $('.product')
     // iterate through each product element.
@@ -39,35 +35,64 @@ async function scrapeData() {
         url: url,
         price: price,
       }
-      //add the product object to the product array
+      //add the product object to the product array.
       products.push(product)
     }
 
-    // Check if the `--export` argument is provided and its value
-    const exportFormat = args.export && args.export.toUpperCase()
-    if (exportFormat === 'JSON') {
-      // Export data to a JSON file
-      const jsonData = JSON.stringify(products)
-      fs.writeFileSync('product.json', jsonData)
-      console.log('Correct export format. Data exported to product.json')
-    } else if (exportFormat === 'CSV') {
-      // Export data to a CSV file
-      const csvData = products
-        .map(
-          (product) =>
-            `${product.name},${product.image},${product.url},${product.price}`,
-        )
-        .join('\n')
-      fs.writeFileSync('product.csv', csvData)
-      console.log('Correct export format. Data exported to product.csv')
-    } else if (exportFormat) {
-      console.error('Invalid export format. Only CSV and JSON are supported.')
-    } else {
-      console.log(products)
-    }
+    console.log(products)
+    await exportData()
   } catch (error) {
     console.error(error)
   }
 }
-
 scrapeData()
+
+// this function exportData using the minimist library
+async function exportData() {
+  // parse command line arguments using minimist
+  const args = minimist(process.argv.slice(1), {
+    string: ['export'], // 'export' as a string argument
+    alias: { e: 'export' }, // '-e' as an alias for 'export'
+  })
+
+  // check if the `--export` argument is provided and its value
+  const exportFormat = args.export && args.export.toUpperCase()
+  // create a folder for the exported files, if it doesn't already exist
+  const folderPath = path.join(process.cwd(), 'export-data')
+  try {
+    await fs.promises.mkdir(folderPath, { recursive: true })
+    console.log(`Directory ${folderPath} found successfully!`)
+  } catch (error) {
+    console.error(`Error creating directory: ${error}`)
+  }
+
+  // export data to a json file
+  if (exportFormat === 'JSON') {
+    const jsonData = JSON.stringify(products)
+    const filePath = path.join(folderPath, 'product.json')
+    try {
+      await fs.promises.writeFile(filePath, jsonData)
+      console.log('Correct export format. Data exported to product.json')
+    } catch (error) {
+      console.log('Error exporting data:', error)
+    }
+
+    // export data to a csv file
+  } else if (exportFormat === 'CSV') {
+    const csvData = `name,image,url,price${os.EOL}${products
+      .map(
+        (product) =>
+          `${product.name},${product.image},${product.url},${product.price}`,
+      )
+      .join(os.EOL)}`
+    const filePath = path.join(folderPath, 'product.csv')
+    try {
+      await fs.promises.writeFile(filePath, csvData)
+      console.log('Correct export format. Data exported to product.csv')
+    } catch (error) {
+      console.log('Error exporting data:', error)
+    }
+  } else if (exportFormat) {
+    console.error('Invalid export format. Only CSV and JSON are supported.')
+  }
+}

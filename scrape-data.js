8b7bce3b3b89310c1c16206678934a9a29c7a853
mudi28 <mudi2000j@gmail.com>
2023-03-10 02:10:28 +0100

    Chapter 7

--- scrape-data.js
+++ scrape-data.js
@@ -4,90 +4,89 @@ import minimist from 'minimist'
 import fs from 'fs'
 import path from 'path'
 import os from 'os'
+
 // set to keep track of visited pages
 const visitedPages = new Set()
+
+// queue to keep track of links to visit
+const linksToVisit = ['https://scrapeme.live/shop/page/1/']
+
 // array to store products
 const products = []
-// function to generate a random page number between 1 and totalPages
-async function getRandomPageNumber(totalPages) {
-  return Math.floor(Math.random() * totalPages) + 1
-}
-// this function scrapes data using the axios and cheerio libraries
-async function scrapeData(pageNumber) {
+
+async function scrapeData() {
   try {
-    // make an HTTP GET request to the specified URL expecting a response
-    const response = await axios.get(
-      `https://scrapeme.live/shop/page/${pageNumber}`,
-    )
-    // extract the HTML content from the request response
-    const html = response.data
-    // loading HTML content in the cheerio document
-    const $ = load(html)
-    // Selects all DOM elements with the class 'product'
-    const productElements = $('.product')
-    // iterate through each product element.
-    for (let i = 0; i < productElements.length; i++) {
-      // extract product name,image,url,price from the element
-      const element = productElements[i]
-      const name = $(element).find('.woocommerce-loop-product__title').text()
-      const image = $(element).find('img').attr('src')
-      const url = $(element)
-        .find('a.woocommerce-LoopProduct-link.woocommerce-loop-product__link')
-        .attr('href')
-      const price = $(element).find('span.price').text()
-      // create a product object containing the extracted information
-      const product = {
-        name: name,
-        image: image,
-        url: url,
-        price: price,
+    // iterate over the links in the queue
+    while (linksToVisit.length > 0) {
+      // get the next link to visit from the front of the queue
+      const link = linksToVisit.shift()
+
+      // check if the link has already been visited
+      if (visitedPages.has(link)) {
+        // if the link has already been visited, skip it and move on to the next link in the queue
+        continue
       }
-      //add the product object to the product array.
-      products.push(product)
+
+      // mark the link as visited
+      visitedPages.add(link)
+
+      // make an HTTP GET request to the specified URL expecting a response
+      const response = await axios.get(link)
+
+      // extract the HTML content from the request response
+      const html = response.data
+
+      // loading HTML content in the cheerio document
+      const $ = load(html)
+
+      // Selects all DOM elements with the class 'product'
+      const productElements = $('.product')
+
+      // iterate through each product element.
+      for (let i = 0; i < productElements.length; i++) {
+        // extract product name,image,url,price from the element
+        const element = productElements[i]
+        const name = $(element).find('.woocommerce-loop-product__title').text()
+        const image = $(element).find('img').attr('src')
+        const url = $(element)
+          .find('a.woocommerce-LoopProduct-link.woocommerce-loop-product__link')
+          .attr('href')
+        const price = $(element).find('span.price').text()
+
+        // create a product object containing the extracted information
+        const product = {
+          name: name,
+          image: image,
+          url: url,
+          price: price,
+        }
+
+        // add the product object to the product array.
+        products.push(product)
+      }
+
+      console.log(`Scraped data from ${link}`)
+      // select all the elements that represent links to the next page
+      const nextPageUrls = $('a.page-numbers')
+      // loop through each of the next page links
+      for (let i = 0; i < nextPageUrls.length; i++) {
+        // get the URL of the current next page link
+        const nextPageUrl = $(nextPageUrls[i]).attr('href')
+        // check if the next page URL is not already in the list of links to visit
+        if (!linksToVisit.includes(nextPageUrl)) {
+          // if the next page URL is not in the list of links to visit, add it to the list
+          linksToVisit.push(nextPageUrl)
+        }
+      }
     }
 
-    console.log(`Page ${pageNumber} done!`)
-    // export the products array to a json/csv file
-    await exportData()
+    // if there are no more links to visit, export the data to a file
+    await exportData(products)
   } catch (error) {
     console.error(error)
   }
 }
-// function to get the total number of pages to be scraped
-async function getTotalPages() {
-  try {
-    // make an HTTP GET request to the specified URL expecting a response
-    const response = await axios.get('https://scrapeme.live/shop/page/1/')
-    // extract the HTML content from the request response
-    const html = response.data
-    // loading HTML content in the cheerio document
-    const $ = load(html)
-    // extract the total number of pages
-    const totalPages = parseInt(
-      $('.page-numbers:not(.prev):not(.next)').last().text(),
-    )
-
-    console.log(totalPages)
-    // loop through random pages until all pages have been visited
-    while (visitedPages.size < totalPages) {
-      // generate a random page number using the `getRandomPageNumber` function
-      const randomPageNumber = await getRandomPageNumber(totalPages)
-      // check if the random page number has not been visited before
-      if (!visitedPages.has(randomPageNumber)) {
-        // add the random page number to the set of visited pages
-        visitedPages.add(randomPageNumber)
-        // call the `scrapeData` function with the random page number as an argument
-        await scrapeData(randomPageNumber)
-      }
-    }
-
-    console.log(`Scraping done for ${visitedPages.size} pages!`)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-getTotalPages()
+scrapeData()
 
 // this function exportData using the minimist library
 async function exportData() {
